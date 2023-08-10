7dcdb23edbb44a92189d3bdc0f8e559e07348653
valencprado <85965282+valencprado@users.noreply.github.com>
2023-08-10 23:19:07 +0000

    refactor: utilizando "this" como boa prática

--- Carro.java
+++ Carro.java
@@ -5,9 +5,9 @@ class Carro {
   String cor;
 
   public void exibirModelo() {
-    System.out.println("O modelo do carro é: " + modelo);
+    System.out.println("O modelo do carro é: " + this.modelo);
   }
   public void exibirAnoECor() {
-    System.out.println("O ano do carro é " + ano + ", enquanto a cor é " + cor);
+    System.out.println("O ano do carro é " + this.ano + ", enquanto a cor é " + this.cor);
   }
 }
